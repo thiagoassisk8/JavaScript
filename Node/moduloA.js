46a437004b2335a2f7ff5a3e0f0dc41436155a8d
thiagoassisk8 <tyagosilva82@gmail.com>
2021-03-09 10:44:42 -0300

    1 commit

---
+++ Node/moduloA.js
@@ -0,0 +1,6 @@
+this.ola = 'Fala pessoal' //Nesse caso o "this" deixa o objeto ou variavel global
+
+let nome = 'thiago' 
+
+exports.bemVindo = 'Bem vindo ao node!' //somente aquilo que eu exporto estará visivel fora do módulo
+module.exports.ateLogo = 'Até o próximo exemplo '
---
+++ Node/moduloB.js
@@ -0,0 +1,6 @@
+module.exports = {
+    BomDia: 'Bom dia',
+    boaNoite(){
+        return 'Boa noite'
+    }
+}
