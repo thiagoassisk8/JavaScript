5d778a6a46c9e958965794a92c31463c1f42dcd5
thiagoassisk8 <tyagosilva82@gmail.com>
2021-03-16 13:40:12 -0300

    pra pegar dps no backup

---
+++ Node/PassandoParametrosCliente.js
@@ -0,0 +1,2 @@
+const parametros = require('./passandoparametros')('thiago','lucas','victor','marcus')
+console.log(parametros)
--- Node/passandoparametros.js
+++ Node/passandoparametros.js
@@ -1,5 +1,19 @@
-nomes = ['Thiago','Lucas','Marcus','Victor']
+// nomes = ['Thiago','Lucas','Marcus','Victor']
+
+const { indexOf } = require("lodash")
 
 module.exports = function(...nomes){
     return nomes.map(nome => `Bom dia ${nome}!`)
+    
 }
+
+const bomdiaguys = function(...nomes){
+    for (let nome of nomes){
+        nome = [`As pessoas que andam de skate é o ${nome}`]
+        // console.log(typeof(nome))
+        // return nome
+        
+    }
+}
+
+bomdiaguys('thiago','marcus','lucas','victor')
